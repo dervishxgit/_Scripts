f23d34d26a55d96ea5ddbc4848087ccb0a7fdcef
SM <dervishx@stu.aii.edu>
2013-04-23 11:57:40 -0700

    core and contoller mappings

--- Creature/Wasp_Controller.cs
+++ Creature/Wasp_Controller.cs
@@ -1,11 +1,36 @@
 using UnityEngine;
 using System.Collections;
 
+//Wasp Controller
+/*
+ * Objective of wasp controller is to direct the physical orientation and location of the wasp creature/geometry
+ * 
+ * Controller should have two modes: 
+ * 		auto: default mode, AI controlled
+ * 		override: direct control mode
+ * 
+ * By default, controller will check to see if it has an existing waypoint, supplied by the mind/core,
+ * if it is ready to go, it will begin the movement operations to reach the location.
+ * If it does not have an existing waypoint it will cast about for one and pick, usually at random
+ * 
+ */
+[System.Serializable]
 public class Wasp_Controller : MonoBehaviour {
-
+	
+	//References
+	
+	public Datacore dCore;
+	public Wasp_Core wCore;
+	public GameObject waspRoot;
+	public GameObject waspGeo;
+	
 	// Use this for initialization
 	void Start () {
-	
+		//establish connection to creature core and global datacore
+		dCore = GameObject.FindGameObjectWithTag("CORE").GetComponent<Datacore>() as Datacore;
+		wCore = gameObject.GetComponent<Wasp_Core>() as Wasp_Core;
+		waspRoot = wCore.waspRoot;
+		waspGeo = wCore.waspGeo;
 	}
 	
 	// Update is called once per frame
--- Creature/Wasp_Core.cs
+++ Creature/Wasp_Core.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 using System.Collections;
 
 public class Wasp_Core : MonoBehaviour {
-
+	
+	//References
+	public Datacore dCore;
+	private Wasp_Core wCore;
+	public GameObject waspRoot;
+	public GameObject waspGeo;
+	
 	// Use this for initialization
 	void Start () {
-	
+		//establish connection to creature core and global datacore
+		dCore = GameObject.FindGameObjectWithTag("CORE").GetComponent<Datacore>() as Datacore;
+		wCore = this;
 	}
 	
 	// Update is called once per frame
