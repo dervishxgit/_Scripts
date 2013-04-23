b6f409de94d849dd3f21bae8f549aec6b5e90aa9
SM <dervishx@stu.aii.edu>
2013-04-23 11:31:20 -0700

    initial wasp scripts created

---
+++ Creature/Wasp_TransmitterChemo.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Wasp_TransmitterChemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
