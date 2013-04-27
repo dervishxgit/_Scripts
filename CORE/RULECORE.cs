6dcd6c87fa95b3f6eaaa05d317fae9916803e531
SM <dervishx@stu.aii.edu>
2013-04-27 12:01:18 -0700

    begin seektarget3d adjustments in rulecore
    not finished

--- CORE/RULECORE.cs
+++ CORE/RULECORE.cs
@@ -10,6 +10,8 @@ public class RULECORE : MonoBehaviour {
 	 * adjust seek target (or create new) for 3D space
 	 */ 
 	
+	//SeekTarget3D settings
+	
 		
 	static public void _RotateYaw(Component bot, float fTurnRate) {
 		if(fTurnRate > 6.0f) fTurnRate = 6.0f;
@@ -27,6 +29,63 @@ public class RULECORE : MonoBehaviour {
 		if(fVelocity > 0.75f) fVelocity = 0.75f;
 		if(fVelocity < -0.75f) fVelocity = -0.75f;
 		bot.transform.Translate(fVelocity * Vector3.forward, Space.Self);	
+	}
+	
+	// _SeekTarget3D : Seeks out the indicated target and returns true when reached (adjusted from original version below, uses settings supplied above)
+	static public bool _SeekTarget3D(Component bot, Vector3 target, float fMaxVelocity) {
+		float fTargetDistance;
+		float zIsTargetBehindMe, zIsTargetInFrontOfMe, zIsTargetToMyLeft, zIsTargetToMyRight, zIsTargetAboveMe, zIsTargetBelowMe;
+		AICORE._GetSpatialAwareness3D(bot, target, out fTargetDistance, 
+			out zIsTargetBehindMe, out zIsTargetInFrontOfMe, 
+			out zIsTargetToMyLeft, out zIsTargetToMyRight, 
+			out zIsTargetAboveMe, out zIsTargetBelowMe);
+		
+		// Detect whether TARGET is sufficiently in front
+		if(zIsTargetInFrontOfMe > 0.99) {
+			// Satisfactally facing target	
+			// No need to turn
+		} else {
+			// Should we turn right or left?
+			if(zIsTargetToMyRight > zIsTargetToMyLeft) {
+				// Turn right
+				float fTurnRate;
+				if(zIsTargetBehindMe > zIsTargetToMyRight) {
+					fTurnRate = AICORE._Defuzzify(zIsTargetBehindMe, 0.0f, 6.0f);					
+				} else {
+					fTurnRate = AICORE._Defuzzify(zIsTargetToMyRight, 0.0f, 6.0f);
+				}
+				RULECORE._RotateYaw(bot, fTurnRate);
+			} else {
+				// Turn left
+				float fTurnRate;
+				if(zIsTargetBehindMe > zIsTargetToMyLeft) {
+					fTurnRate = AICORE._Defuzzify(zIsTargetBehindMe, 0.0f, 6.0f);					
+				} else {
+					fTurnRate = AICORE._Defuzzify(zIsTargetToMyLeft, 0.0f, 6.0f);
+				}
+				RULECORE._RotateYaw(bot, -fTurnRate);
+			}
+		}
+					
+		if(fMaxVelocity > 0.0f) {
+			// Only drive forward when facing nearly toward target	
+			if(zIsTargetInFrontOfMe > 0.7) {
+				// Only drive forward if we're far enough from target
+				if(fTargetDistance >= 3.00f) {
+					float fVelocity = AICORE._Defuzzify(zIsTargetInFrontOfMe, 0.0f, fMaxVelocity);
+					RULECORE._MoveForward(bot, fVelocity);
+				}
+			}
+			
+			// Return whether target is reached
+			return fTargetDistance < 3.00f;
+		} else {
+			// Return whether we're facing the target
+			// Also include whether target is reached because when
+			// we're very close to the target we get weird look at information
+			return zIsTargetInFrontOfMe > 0.9f || fTargetDistance < 5.00f;
+		}
+		
 	}
 	
 	// _SeekTarget : Seeks out the indicated target and returns true when reached
