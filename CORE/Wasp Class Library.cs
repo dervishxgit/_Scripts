7a26e3acb6576c64493cc80867c6c5e6ad999e6a
sm <dervishx@gmail.com>
2013-05-05 14:26:29 -0700

    made copy of chemo in receptor after leaving zone. /n working on _Chemo_ decay

--- CORE/Wasp Class Library.cs
+++ CORE/Wasp Class Library.cs
@@ -2,53 +2,69 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class _TestClass_ {
+public class _TestClass_
+{
 	
 };
 
-public class _Action_ {
+public class _Action_
+{
 		
-	};
+};
 	
-	public class _Role_ {
-		string sName;
-		
-		List<_Action_> _lActions = new List<_Action_>();
-	};
+public class _Role_
+{
+	string sName;
+	List<_Action_> _lActions = new List<_Action_> ();
+};
 
-public class _Chemo_ {
-		public Color chemoColor = Color.black;
-		float fInitialMemTime, fCurrentMemTime;
-		float fMemDecayRate = 1.0f;
-		float fDefaultInitialMemTime = 30.0f; //default seconds for Chemo to persist in memory
+public class _Chemo_
+{
+	public Color chemoColor = Color.black;
+	float fInitialMemTime, fCurrentMemTime;
+	float fMemDecayRate = 1.0f;
+	float fDefaultInitialMemTime = 30.0f; //default seconds for Chemo to persist in memory
 		
-		public _Chemo_() {
-			fInitialMemTime = fDefaultInitialMemTime;
-		}
+	public _Chemo_ ()
+	{
+		fInitialMemTime = fCurrentMemTime = fDefaultInitialMemTime;
+	}
 		
-		public _Chemo_( Color color ) {
-			
-		}
+	public _Chemo_ (Color color)
+	{
+		fInitialMemTime = fCurrentMemTime = fDefaultInitialMemTime;
+		chemoColor = color;
+	}
 		
-		public _Chemo_( _Chemo_ chem ) {
-			
-		}
+	public _Chemo_ (_Chemo_ chem)
+	{
+		chemoColor = chem.chemoColor;
+		fInitialMemTime = chem.fInitialMemTime;
+		fCurrentMemTime = chem.fCurrentMemTime;
+	}
 		
-		bool _Decay(float amount) {
-			fCurrentMemTime -= amount * fMemDecayRate;
-			return fCurrentMemTime <= 0.0f;
-		}
+	bool _Decay (float amount)
+	{
+		fCurrentMemTime = fCurrentMemTime - amount;// * fMemDecayRate;
+		return fCurrentMemTime <= 0.0f;
+	}
 		
-		public void _Run(float fTime, out bool bExpired) {
-			bExpired = _Decay(fTime);
-		}
+	public void _Run (float fTime, out bool bExpired)
+	{
+		bExpired = _Decay (fTime);
+	}
 		
-		public void _Refresh() {
-			fCurrentMemTime = fInitialMemTime;
-		}
+	public void _Refresh ()
+	{
+		fCurrentMemTime = fInitialMemTime;
+	}
+	
+	public float _GetCurrentMemTime() {
+		return fCurrentMemTime;
+	}
 		
 		
-	};
+};
 
 //public class WaspClassLibrary : MonoBehaviour {
 //
--- Creature/Wasp_Chemoreceptor.cs
+++ Creature/Wasp_Chemoreceptor.cs
@@ -1,58 +1,70 @@
 using UnityEngine;
 using System.Collections;
 
-public class Wasp_Chemoreceptor : MonoBehaviour {
+public class Wasp_Chemoreceptor : MonoBehaviour
+{
 	
 	public Wasp_Core wCore;
-	
 	public _Chemo_ currentChemo;
-	
 	public Color currentChemoColor;
-	
 	public float fTriggered = 0.0f;
 	public bool bTriggered = false;
 	float fTriggeredTimeThreshold = 1.0f;
+	float fReceptorDecayRate = 0.1f;
 	
 	//should only trigger on "Chemo" Layer
-	void OnTriggerEnter(Collider other) {
+	void OnTriggerEnter (Collider other)
+	{
 		
 	}
 	
-	void OnTriggerStay(Collider other) {
-		currentChemo = other.transform.root.gameObject.GetComponent<ChemoBehavior>()._Chemo;
+	void OnTriggerStay (Collider other)
+	{
+		currentChemo = other.transform.root.gameObject.GetComponent<ChemoBehavior> ()._Chemo;
 		currentChemoColor = currentChemo.chemoColor;
 		
 		bTriggered = true;
 		fTriggered = 0.0f;
-		Debug.Log("did trigger");
+		//Debug.Log ("did trigger");
 	}
 	
-	void OnTriggerExit(Collider other) {
+	void OnTriggerExit (Collider other)
+	{
 		//bTriggered = false;
+		
+		//we've left the chemo zone, make a copy of the sensation
+		currentChemo = new _Chemo_ (other.transform.root.gameObject.GetComponent<ChemoBehavior> ()._Chemo);
 	}
 	
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		//map to wasp core no matter where we are in hierarchy
-		wCore = transform.root.gameObject.GetComponent<Wasp_Core>();
+		wCore = transform.root.gameObject.GetComponent<Wasp_Core> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		fTriggered += Time.deltaTime;
-		if(fTriggered > fTriggeredTimeThreshold) {
+		if (fTriggered > fTriggeredTimeThreshold) {
 			bTriggered = false;
 		}
-		if(currentChemo != null) {
-			wCore._UpdateCurrentChemo(currentChemo);
+		if (currentChemo != null) {
+			wCore._UpdateCurrentChemo (currentChemo);
 			currentChemoColor = currentChemo.chemoColor;
 			
-			if(!bTriggered) {
-				//decay our chemocolor
-				currentChemo.chemoColor *= 0.9f;
-				//currentChemoColor *= 0.9f;
+			if (bTriggered == false) {
+				bool expired;
+				currentChemo._Run (0.1f, out expired);
+				Debug.Log (currentChemo._GetCurrentMemTime ());
+				//not smelling
+				if (expired) {
+					currentChemo = null;
+					currentChemoColor = Color.black;
+				}
 			}
-		}
+		} 
 		
 	}
 }
