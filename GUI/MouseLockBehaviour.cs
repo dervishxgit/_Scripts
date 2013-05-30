eb6e330fb5ab8e542a0d8f4e806323412e324371
dervishxgit <dervishx@gmail.com>
2013-05-30 15:05:32 -0700

    added mouse locking based on menu display

---
+++ GUI/MouseLockBehaviour.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class MouseLockBehaviour : MonoBehaviour {
+    void DidLockCursor() {
+        Debug.Log("Locking cursor");
+        //guiTexture.enabled = false;
+    }
+    void DidUnlockCursor() {
+        Debug.Log("Unlocking cursor");
+        //guiTexture.enabled = true;
+    }
+    void OnMouseDown() {
+        Screen.lockCursor = true;
+    }
+    private bool wasLocked = false;
+    void Update() {
+//        if (Input.GetKeyDown("escape"))
+//            Screen.lockCursor = false;
+        
+		if(Datacore.bDisplayAllMenus && wasLocked) {
+			Screen.lockCursor = false;
+			Debug.Log("unlocked mouse cursor");
+		} else if (!Datacore.bDisplayAllMenus && !wasLocked) {
+			Screen.lockCursor = true;
+			Debug.Log("locked mouse cursor");
+		}
+		
+        if (!Screen.lockCursor && wasLocked) {
+            wasLocked = false;
+            DidUnlockCursor();
+        } else
+            if (Screen.lockCursor && !wasLocked) {
+                wasLocked = true;
+                DidLockCursor();
+            }
+    }
+}
