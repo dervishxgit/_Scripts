11976ed78e68b5b74485ae33672815503b760a05
steve m <devishx@gmail.com>
2013-06-03 21:15:58 -0700

    fudgin with inspector

---
+++ GUI/InspectableObjectBehaviour.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InspectableObjectBehaviour : MonoBehaviour {
+
+    bool bUpdatingStrings = false;
+
+    float fUpdateInterval = 1.0f;
+
+    public List<string> lsProperties = new List<string>();
+
+    public List<string> lsValues = new List<string>();
+
+    public int inspectableObjectType = 0; //set in editor
+    const int
+        typeTest = 0,
+        typeWasp = 1,
+        typeHive = 2,
+        typePlant = 3;
+
+    public struct stInspectorStrings {
+       public const string 
+            p_name = "Name: ",
+            p_type = "Type: ",
+            p_state = "State: ";
+       public string
+           v_name,
+           v_type,
+           v_state;
+
+       //public string[] aProps;
+       //public string[] aVals;
+    };
+
+    public stInspectorStrings _stins;
+
+    void Awake() {
+        BuildPropertiesList();
+        switch(inspectableObjectType) {
+            case typeTest:
+                _stins.v_name = gameObject.name;
+                _stins.v_type = "TEST";
+                _stins.v_state = "testing...";
+                break;
+
+            case typeWasp:
+                _stins.v_name = gameObject.name;
+                _stins.v_type = "WASP";
+                _stins.v_state = gameObject.GetComponent<Wasp_Core>().stateOfMind.ToString();
+                break;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!bUpdatingStrings) {
+            StartCoroutine(UpdateStrings());
+        }
+	}
+
+    IEnumerator UpdateStrings() {
+        bUpdatingStrings = true;
+
+        switch (inspectableObjectType)
+        {
+            case typeWasp:
+                _stins.v_name = gameObject.name;
+                _stins.v_type = "WASP";
+                _stins.v_state = gameObject.GetComponent<Wasp_Core>().stateOfMind.ToString();
+                break;
+        }
+
+        yield return new WaitForSeconds(fUpdateInterval);
+        bUpdatingStrings = false;
+    }
+
+    void BuildPropertiesList() {
+        
+    }
+}
--- GUI/InspectorMenuBehaviour.cs
+++ GUI/InspectorMenuBehaviour.cs
@@ -6,12 +6,17 @@ public class InspectorMenuBehaviour : MonoBehaviour {
 
     public GUIText inspectorGUIText;
 
+    InspectableObjectBehaviour inspectableObject;
+
     public bool bDisplay = true;
+    bool btest = false;
 
     const int inspectorX = 300;
     const int inspectorY = 400;
 
     List<string> lStrings = new List<string>();
+
+
 
     const string 
         test00 = "\n",
@@ -21,6 +26,12 @@ public class InspectorMenuBehaviour : MonoBehaviour {
 
     static Rect inspectorWindowRect;
 
+    string inspectorWindowText;
+
+    void Awake () {
+        inspectableObject = gameObject.GetComponent<InspectableObjectBehaviour>();
+    }
+
 	// Use this for initialization
 	void Start () {
         //inspectorWindowRect = new Rect(Screen.width - inspectorX,
@@ -29,49 +40,107 @@ public class InspectorMenuBehaviour : MonoBehaviour {
         //inspectorY);
 
         inspectorWindowRect = new Rect(
-            100,
-            100,
-            400,
-            300
+            Screen.width - inspectorX,
+            Screen.height/2,
+            inspectorX,
+            inspectorY
             );
 
         lStrings.Add(test01);
         lStrings.Add(test02);
         lStrings.Add(test03);
 
-        
+        //lsProperties.Add(test01);
+        //lsProperties.Add(test02);
+        //lsProperties.Add(test03);
+
+        //lsValues.Add(test01);
+        //lsValues.Add(test02);
+        //lsValues.Add(test03);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (bDisplay) {
-            do
-            {
-                StartCoroutine(DisplayInspectorText());
-            } while (false);
+            //do
+            //{
+            //    StartCoroutine(DisplayInspectorText());
+            //} while (false);
+            if (!btest) {
+                ClearInspectorText();
+                StartCoroutine(DisplayInspectorText(inspectableObject));
+            }
         }
 	}
 
     void OnEnable()
     {
-        StartCoroutine(DisplayInspectorText());
+        //StartCoroutine(DisplayInspectorText(inspectableObject));
     }
 
-    IEnumerator DisplayInspectorText() {
-        inspectorGUIText.text =
-            test01 + test00 +
-            test02 + test00 +
-            test03 + test00;
-        yield return new WaitForEndOfFrame();
+    //IEnumerator DisplayInspectorText() {
+    //    inspectorGUIText.text =
+    //        test01 + test00 +
+    //        test02 + test00 +
+    //        test03 + test00;
+
+    //    inspectorWindowText =
+    //        test01 + test00 +
+    //        test02 + test00 +
+    //        test03 + test00;
+    //    yield return new WaitForEndOfFrame();
+    //}
+
+    //IEnumerator DisplayInspectorText()
+    //{
+    //    foreach (string s in lStrings) {
+    //        inspectorWindowText += s + "\n";
+    //        yield return new WaitForSeconds(0.2f);
+    //    }
+    //}
+
+    IEnumerator DisplayInspectorText(InspectableObjectBehaviour inspect)
+    {
+        //string[] sprops = new string[lsProperties.Count];
+        //string[] svals = new string[lsValues.Count];
+
+        btest = true;
+
+        string[] sprops = inspect.lsProperties.ToArray();
+        string[] svals = inspect.lsValues.ToArray();
+
+        for (int i = 0; i < sprops.Length; i++) { 
+            inspectorWindowText += sprops[i] + " " + svals[i] + "\n";
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        yield return new WaitForSeconds(5.0f);
+        btest = false;
     }
+
+    void ClearInspectorText() {
+        inspectorWindowText = "";
+    }
+
+    //public void _SetProperties(List<string> props) {
+    //    lsProperties = props;
+    //}
+
+    //public void _SetValues(List<string> vals) {
+    //    lsValues = vals;
+    //}
 
     void OnGUI()
     {
         GUILayout.BeginArea(inspectorWindowRect);
-        GUILayout.Box("Inspector");
+        GUILayout.Box(inspectorWindowText);
         //GUILayout.BeginHorizontal();
         //GUILayout.EndHorizontal();
         GUILayout.EndArea();
         //GUILayout.te
+    }
+
+    void OnMouseDown() { 
+        
     }
 }
